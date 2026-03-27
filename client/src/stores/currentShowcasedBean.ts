a5dc6991b51b5ddbde87fb27023a467354b4963c
AidanBoissonneault <58098123+AidanBoissonneault@users.noreply.github.com>
2026-03-27 12:49:39 -0400

    Added default state to currentBeanStore

--- client/src/stores/currentShowcasedBean.ts
+++ client/src/stores/currentShowcasedBean.ts
@@ -1,9 +1,18 @@
 import { defineStore } from 'pinia'
 import { type Bean } from '@/types'
 
+const defaultBean: Bean = {
+	name: "Error Loading Bean",
+	variety: "Go back to dashboard and reload.",
+	status: "finished",
+	pri_hue: 0,
+	sec_hue: 10,
+	acc_hue: 20,
+}
+
 export const useCurrentBeanStore = defineStore('currentBean', {
 	state: () => ({
-		currentBean: <Bean>{}
+		currentBean: defaultBean
 	}),
 
 	actions: {
