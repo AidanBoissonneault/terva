962be3625d9ded05aac5aa758a38b74ed3f5bbd2
AidanBoissonneault <58098123+AidanBoissonneault@users.noreply.github.com>
2026-04-09 16:12:15 -0400

    Built with changed authClient based URL

--- packages/client/src/lib/auth-client.ts
+++ packages/client/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/vue";
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL ?? "https://www.tervabrewed.com",
 });
 
 // Named exports for convenience
