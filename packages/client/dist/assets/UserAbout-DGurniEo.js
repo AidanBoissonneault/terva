e5fae9886c6e605cdf12b7db883cb656a147a672
AidanBoissonneault <58098123+AidanBoissonneault@users.noreply.github.com>
2026-04-15 11:47:39 -0400

    Reverted end brew screen

--- packages/client/dist/assets/UserAbout-DGurniEo.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-BsEVMij0.js";const o={};function n(r,s){return t(),c("div",null,"testing About")}const a=e(o,[["render",n]]);export{a as default};
---
+++ packages/client/dist/assets/UserAbout-DQ226g04.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-BQt5K5v6.js";const o={};function n(r,s){return t(),c("div",null,"testing About")}const a=e(o,[["render",n]]);export{a as default};
